3ed4e8a31099176c3a2c8c24f83c3a9e7c71dc69
MikkoP <penttila.mikko@gmail.com>
2024-03-02 20:27:07 +0200

    Made observer example more clear and added small expansion to it.

--- gof/3_behavioral/1_observer.py
+++ gof/3_behavioral/1_observer.py
@@ -8,51 +8,82 @@ Examples:
 '''
 from abc import ABC, abstractmethod
 
-class Subscriber(ABC):
+class Observer(ABC):
+    """Abstract Observer class that will be implemented later."""
 
     @abstractmethod
-    def update(self, message):
+    def update(self, message: str):
         pass
 
 class Publisher:
+    """Class for the Publisher.
+    
+    Holds a list of observers and provides functionality to add, remove and notify the 
+    observers. This is a one-to-many relationship. One publisher can be followed by 
+    many subscribers.
+    """
     def __init__(self):
-        self._subscribers: list[Subscriber] = []  # This where references to all the observers are being kept
-                                                  # This is a one-to-many relationship: one publisher can be
-                                                  # followed by many subscribers
+        """Initialize the Publisher object with an empty list of observers."""
+        self._observers: list[Observer] = []
 
-    def add_subscriber(self, subsrciber: Subscriber):
-        # Add the observer to the list if it is not already there
-        if subsrciber not in self._subscribers:
-            self._subscribers.append(subsrciber)
+    def add_observer(self, observer: Observer):
+        """Add an observer to the list if it is not already there"""
+        if observer not in self._observers:
+            self._observers.append(observer)
 
-    def remove_subscriber(self, subscriber: Subscriber):
-        self._subscribers.remove(subscriber)
+    def remove_observer(self, observer: Observer):
+        """Remove an observer from the list."""
+        self._observers.remove(observer)
 
-    def notify_subscribers(self, message: str):
-        # Trigger the update for all observers
-        for subscriber in self._subscribers:
-            subscriber.update(message)
+    def notify_observers(self, message: str):
+        """Trigger the update method of all observers."""
+        for observer in self._observers:
+            observer.update(message)
 
-# Observer implementation
-class SubscriberImplementation(Subscriber):
+class ObserverImplementationA(Observer):
+    """Implementation of the Observer class.
+
+    This is the actual class that will be added to the publishers list.
+    """
     def __init__(self, subscriber_name: str):
         self.subscriber_name = subscriber_name
 
     def update(self, message: str):
+        """Holds the logic of what the observer should do when it is notified."""
         print(f"{self.subscriber_name}: {message}")
 
-# Example usage
-event_publisher = Publisher()
+class ObserverImplementationB(Observer):
+    """A different implementation.
 
-sub_a = SubscriberImplementation("Subscriber A")
-sub_b = SubscriberImplementation("Subscriber B")
+    It can still observe the Publisher because it inherits the Observer.
+    """
+    def __init__(self, subscriber_name: str, data: str):
+        self.subscriber_name = subscriber_name
+        self.data = data
 
-event_publisher.add_subscriber(sub_a)
-event_publisher.add_subscriber(sub_b)
+    def update(self, message: str):
+        """This is again a different implementation."""
+        if message == "Event 2":
+            self.data = "Data was modified"
 
-event_publisher.notify_subscribers("Event 1\n")
+        print(f"{self.subscriber_name} has data:\n{self.data}")
 
-event_publisher.remove_subscriber(sub_a)
 
-event_publisher.notify_subscribers("Event 2\n")
 
+if __name__ == "__main__":
+    event_publisher = Publisher()
+
+    observer_a = ObserverImplementationA("Observer A")
+    observer_b = ObserverImplementationA("Observer B")
+    observer_c = ObserverImplementationB("Observer C", "Some data")
+
+    event_publisher.add_observer(observer_a)
+    event_publisher.add_observer(observer_b)
+    event_publisher.add_observer(observer_c)
+
+    event_publisher.notify_observers("Event 1")
+    print()
+
+    event_publisher.remove_observer(observer_a)
+
+    event_publisher.notify_observers("Event 2")
